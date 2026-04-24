8420a3afc5c5f219bc42837bbe63bdece7cd8fd6
unknown <guilhermefariareis@gmail.com>
2026-04-24 10:17:56 -0300

    fix: make numeric backspace larger for better touch accuracy

--- script.js
+++ script.js
@@ -1343,7 +1343,7 @@ function openKeyboard() {
     grid.className = isPhone ? 'keyboard-grid numeric' : 'keyboard-grid';
 
     const layout = isPhone 
-        ? ['1', '2', '3', '4', '5', '6', '7', '8', '9', '', '0', '⌫']
+        ? ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '⌫']
         : [
             'Q','W','E','R','T','Y','U','I','O','P',
             'A','S','D','F','G','H','J','K','L','Ç',
